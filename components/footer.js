f2230e211d75758a31ff8bb22d0bfbeac3295e3a
Alester Mafra <alester.mafra@gmail.com>
2022-05-29 19:06:52 -0300

    Fixes

--- components/footer.js
+++ components/footer.js
@@ -5,11 +5,11 @@ export default function Footer() {
     <footer>
       <div className="container">
         <div className="text-center py-4">
-          <a href="https://instagram.com/megablack_sp" className="btn text-white fs-4" target="_blank">
+          <a href="https://instagram.com/megablack_sp" className="btn text-white fs-4" target="_blank" rel="noreferrer">
             <i className="bi bi-instagram"></i>
           </a>
 
-          <a href="https://www.youtube.com/c/MegaBlack_SP" className="btn text-white fs-4" target="_blank">
+          <a href="https://www.youtube.com/c/MegaBlack_SP" className="btn text-white fs-4" target="_blank" rel="noreferrer">
             <i className="bi bi-youtube"></i>
           </a>
         </div>
--- components/upcoming-events.js
+++ components/upcoming-events.js
@@ -16,18 +16,18 @@ function Event({ event }) {
                 <h2 className="pb-3">
                     <span className="text-uppercase">{event.name}</span>
                     {' '}
-                    <a href={"https://instagram.com/" + event.instagram} className="text-decoration-none text-white" target="_blank">
-                        <span class="fs-5 text-secondary"><span class="text-danger">@</span>{event.instagram}</span>
+                    <a href={"https://instagram.com/" + event.instagram} className="text-decoration-none text-white" target="_blank" rel="noreferrer">
+                        <span className="fs-5 text-secondary"><span className="text-danger">@</span>{event.instagram}</span>
                     </a>
                 </h2>
 
                 <div className="fs-6 mb-3">
-                    <i class="bi bi-calendar-event text-danger"></i>
-                    <span class="ms-3 text-uppercase">{event.date}</span>
+                    <i className="bi bi-calendar-event text-danger"></i>
+                    <span className="ms-3 text-uppercase">{event.date}</span>
                 </div>
 
                 <div className="fs-6 mb-3">
-                    <i class="bi bi-geo-alt-fill text-danger"></i>
+                    <i className="bi bi-geo-alt-fill text-danger"></i>
                     <span className="ms-3">{event.address}</span>
                 </div>
 
